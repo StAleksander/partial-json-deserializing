d89cb8d9357f1b65a30a78d5c4740e799ad42015
Alexander <St.Aleksander@gmail.com>
2018-08-22 23:53:00 +0300

    Add solution,deserializer lib project,unit tests project

---
+++ PartialDeserializer.UnitTests/Models/Orders.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PartialDeserializer.UnitTests.Models
+{
+    public enum PayType
+    {
+        Cash,
+        Card
+    }
+
+    public class Order
+    {
+        public ICollection<OrderItem> Items { get; set; }
+
+        public int OrderId { get; set; }
+            
+        public DateTime PaidTime { get; set; }
+
+        public СashierInfo CashierInfo { get; set; }
+            
+        public PayType PayType { get; set; }
+    }
+
+    public class СashierInfo
+    {
+        public string FullName { get; set; }
+            
+        public int EmployeeId { get; set; }
+            
+        public int ShiftId { get; set; }
+    }
+        
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+            
+        public int GoodId { get; set; }
+            
+        public int Quantity { get; set; }
+            
+        public double Commission { get; set; }
+            
+        public double Price { get; set; }
+    }
+
+}
---
+++ PartialDeserializer/PartialDeserializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartialDeserializer
+{
+    public static class PartialDeserializer
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return default(T);
+        }
+    }
+}
